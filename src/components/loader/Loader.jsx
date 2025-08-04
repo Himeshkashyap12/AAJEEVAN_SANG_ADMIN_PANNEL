ecbc9c4b2f37a1f63117db390ab8184f9a47e4a6
apple <himesh.kashyap@celestialitverse.com>
2025-08-04 17:22:01 +0530

    loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,17 +1,11 @@
-import {  Vortex } from "react-loader-spinner"
 
+import "./loader.css"
 const Loader=()=>{
     return(
-        <div className="loader absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center h-[70vh]">
-       <Vortex
-  visible={true}
-  height="80"
-  width="80"
-  ariaLabel="vortex-loading"
-  wrapperStyle={{}}
-  wrapperClass="vortex-wrapper"
-  colors={["#F81B3E","#F81B3E","#F81B3E","#F81B3E","#F81B3E","#F81B3E","#F81B3E"]}
-  />
+        <div className="loader absolute top-0 bottom-0 left-0 right-0 flex justify-center items-center h-[70vh] w-full">
+     <div className="loader-container">
+      <div className="spinner" />
+    </div>
       </div>
       
     )
